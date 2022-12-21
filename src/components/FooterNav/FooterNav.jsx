90f3a82d253a87f2d9a01019c4627e4e83437c06
Victoria Mertens <victoria.mertens44@gmail.com>
2022-12-21 14:51:03 -0600

    FooterNav has clickable items, ready for useHistory

--- src/components/FooterNav/FooterNav.jsx
+++ src/components/FooterNav/FooterNav.jsx
@@ -1,5 +1,37 @@
+import { useHistory } from 'react-router-dom';
+
 function FooterNav() {
-  return <p>Testing Footer Nav</p>;
+  const history = useHistory();
+
+  function toCloset() {
+    console.log('To closet');
+  }
+  function toAddItem() {
+    console.log('To addItem');
+  }
+  function toMakeOutfit() {
+    console.log('To Make outfit');
+  }
+  function toOutfits() {
+    console.log('To outfits');
+  }
+
+  return (
+    <div>
+      <div>
+        <p onClick={toCloset}>Closet</p>
+      </div>
+      <div>
+        <p onClick={toAddItem}>Add Item</p>
+      </div>
+      <div>
+        <p onClick={toMakeOutfit}>Make Outfit</p>
+      </div>
+      <div>
+        <p onClick={toOutfits}>Outfits</p>
+      </div>
+    </div>
+  );
 }
 
 export default FooterNav;
