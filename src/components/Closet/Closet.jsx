767161a8c18f2ae7d72544614cb1b44b11e0e065
Victoria Mertens <victoria.mertens44@gmail.com>
2022-12-22 10:03:39 -0600

    Closet component set up for sagas GET

--- src/components/Closet/Closet.jsx
+++ src/components/Closet/Closet.jsx
@@ -1,4 +1,15 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 function Closet() {
+  const dispatch = useDispatch();
+  const store = useSelector((store) => store);
+  console.log('The store:', store);
+
+  useEffect(() => {
+    dispatch({ type: 'GET_CLOSET' });
+  }, []);
+
   return <p>Testing Closet component</p>;
 }
 
