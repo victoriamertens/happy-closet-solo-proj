f450ec652cd95a763ff5be37bbc1575482ca24ca
Victoria Mertens <victoria.mertens44@gmail.com>
2022-12-22 11:30:42 -0600

    Closet images rendering in clsoet component properly

--- src/components/Closet/Closet.jsx
+++ src/components/Closet/Closet.jsx
@@ -3,14 +3,26 @@ import { useDispatch, useSelector } from 'react-redux';
 
 function Closet() {
   const dispatch = useDispatch();
-  const store = useSelector((store) => store);
-  console.log('The store:', store);
+  const closet = useSelector((store) => store.closet);
+  console.log('The store:', closet);
 
   useEffect(() => {
     dispatch({ type: 'GET_CLOSET' });
   }, []);
-
-  return <p>Testing Closet component</p>;
+  if (!closet) {
+    return <p>Opening closet doors...</p>;
+  }
+  if (closet) {
+    console.log('in closet conditional');
+    return (
+      <div className="closet-items">
+        {closet.map((item) => {
+          console.log('Here it is!', item);
+          return <img key={item.id} src={item.image_url}></img>;
+        })}
+      </div>
+    );
+  }
 }
 
 export default Closet;
