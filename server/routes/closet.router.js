5f1592f1c21caf7636558ab358c08a2bbe908804
Victoria Mertens <victoria.mertens44@gmail.com>
2022-12-22 11:02:14 -0600

    Added dummy data to database, router effectively retriving data and sending back to client

--- server/routes/closet.router.js
+++ server/routes/closet.router.js
@@ -7,8 +7,22 @@ const router = express.Router();
  */
 router.get('/', (req, res) => {
   // GET route code here
-  console.log('In closet GET router');
-  res.send({ id: 1, name: 'Susan', color: 'red' });
+  console.log('In closet GET router:', req.user);
+  let queryTextGet = `
+SELECT * 
+FROM "items" 
+WHERE "user_id" = $1; `;
+
+  pool
+    .query(queryTextGet, [req.user.id])
+    .then((response) => {
+      console.log(response.rows);
+      res.send(response.rows);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(500);
+    });
 });
 
 /**
@@ -16,6 +30,9 @@ router.get('/', (req, res) => {
  */
 router.post('/', (req, res) => {
   // POST route code here
+  let queryText = `INSERT INTO "items"
+  ("user_id","name", "color", "cost", "brand", "category_name", "image_url")
+  VALUES ($1, $2, $3, $4, $5, $6, $7);`;
 });
 
 module.exports = router;
