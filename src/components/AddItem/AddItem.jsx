42b2a0dad5fff312cf8e57a7d2fa33961a7c72df
Victoria Mertens <victoria.mertens44@gmail.com>
2022-12-22 13:29:28 -0600

    Add Item form built, onSubmit function hooked up

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -1,5 +1,26 @@
+import { useState } from 'react';
+
 function AddItem() {
-  return <p>testing add item page</p>;
+  const onSubmit = () => {
+    event.preventDefault();
+    console.log('Submitting item');
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <label for="name">
+          <h2>What is your item's Name?</h2>
+        </label>
+        <input id="name" type="text"></input>
+        <p>
+          Get Creative! This can be anything from 'Grandma's Vintage Sweater' to
+          'Cassandra'.
+        </p>
+        <button type="submit">Next</button>
+      </form>
+    </div>
+  );
 }
 
 export default AddItem;
