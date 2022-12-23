2bc86564bf051276cf236f26577f3c59af1eeca0
Victoria Mertens <victoria.mertens44@gmail.com>
2022-12-23 13:00:31 -0600

    Make Outfit comment page completely rendered. Need to add info to store.

--- src/components/MakeOutfitComment/MakeOutfitComment.jsx
+++ src/components/MakeOutfitComment/MakeOutfitComment.jsx
@@ -1,5 +1,47 @@
+import { useState } from 'react';
+
 function MakeOutfitComment() {
-  return <p>Testing outfit comment</p>;
+  const [reaction, setReaction] = useState(0);
+  const [comment, setComment] = useState('');
+
+  function smileReaction() {
+    setReaction(1);
+  }
+  function lineReaction() {
+    setReaction(2);
+  }
+  function frownReaction() {
+    setReaction(3);
+  }
+
+  function setToStore() {
+    console.log('Comment:', comment, 'REaction:', reaction);
+  }
+
+  return (
+    <div>
+      <p>*Reaction: </p>
+      <button class="reaction" onClick={smileReaction}>
+        :)
+      </button>
+      <button class="reaction" onClick={lineReaction}>
+        :/
+      </button>
+      <button class="reaction" onClick={frownReaction}>
+        :(
+      </button>
+      <textarea
+        id="comment"
+        name="comment"
+        rows="4"
+        cols="50"
+        placeholder="How was your experience wearing this outfit? Is it comfortable? Did it
+        help you feel confident? Does it need to be altered?"
+        onChange={(event) => setComment(event.target.value)}
+      ></textarea>
+      <button onClick={setToStore}>Next</button>
+    </div>
+  );
 }
 
 export default MakeOutfitComment;
