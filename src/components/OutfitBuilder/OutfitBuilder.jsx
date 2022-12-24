4974fbf88b07c2def3a46ee2757f4b0a966fde57
Victoria Mertens <victoria.mertens44@gmail.com>
2022-12-24 10:54:40 -0600

    Added basic CSS styling for function of app

--- src/components/OutfitBuilder/OutfitBuilder.jsx
+++ src/components/OutfitBuilder/OutfitBuilder.jsx
@@ -1,6 +1,8 @@
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
+import './OutfitBuilder.css';
+
 function OutfitBuilder() {
   const outfitItems = useSelector((store) => store.newOutfit);
   const history = useHistory();
@@ -12,18 +14,26 @@ function OutfitBuilder() {
 
   if (outfitItems.length === 0) {
     return (
-      <div id="outfit-builder">
+      <div class="outfit-builder">
         <p>Testing empty store</p>
       </div>
     );
   }
 
   return (
-    <div>
-      {outfitItems.map((item) => {
-        return <img src={item.image}></img>;
-      })}
-      <button onClick={nextStep}>Next</button>
+    <div class="outfit-builder">
+      <div id="builder-images">
+        {outfitItems.map((item) => {
+          return (
+            <div class="img-card">
+              <img src={item.image}></img>
+            </div>
+          );
+        })}
+      </div>
+      <div id="builder-button">
+        <button onClick={nextStep}>Next</button>
+      </div>
     </div>
   );
 }
