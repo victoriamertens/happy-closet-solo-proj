0c624ce3e5c4925a4e09b8b385efc0a2ddc8245e
Victoria Mertens <victoria.mertens44@gmail.com>
2023-01-02 10:09:12 -0600

    OnClick of the component, runs function

--- src/components/ClosetItem/ClosetItem.jsx
+++ src/components/ClosetItem/ClosetItem.jsx
@@ -1,7 +1,15 @@
+import { useHistory } from 'react-router-dom';
+
 function ClosetItem({ item }) {
   console.log('ITEM:', item.name);
+  const history = useHistory();
+
+  function navigateDetails() {
+    console.log('in Navigate details, id:', item.id);
+  }
+
   return (
-    <div className="single-item">
+    <div className="single-item" onClick={navigateDetails}>
       <p>{item.name}</p>
       <img key={item.id} src={item.image_url}></img>
     </div>
