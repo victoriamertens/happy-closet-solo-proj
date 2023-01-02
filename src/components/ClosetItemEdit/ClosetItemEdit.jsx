5aade555bf46f92a808c83256a5e18daacb71842
Victoria Mertens <victoria.mertens44@gmail.com>
2023-01-02 13:25:54 -0600

    Input captured by state onChange, Update btn running an update function

--- src/components/ClosetItemEdit/ClosetItemEdit.jsx
+++ src/components/ClosetItemEdit/ClosetItemEdit.jsx
@@ -3,11 +3,20 @@ import { useState } from 'react';
 function ClosetItemEdit(props) {
   console.log(props);
   const [editBtn, setEditBtn] = useState(false);
-  const [detail, setDetail] = useState(props.data);
+  const [detail, setDetail] = useState('');
 
   function editField() {
     console.log('in edit field:', props.field);
     setEditBtn(!editBtn);
+  }
+
+  function handleChange(event) {
+    console.log(detail);
+    setDetail(event.target.value);
+  }
+
+  function updateField() {
+    console.log('Updating Field:', detail, props.field);
   }
 
   if (!editBtn) {
@@ -25,8 +34,12 @@ function ClosetItemEdit(props) {
     return (
       <div>
         <label for={props.field}>{props.field}</label>
-        <input id={props.field} placeholder={props.data}></input>
-        <button>Edit</button>
+        <input
+          id={props.field}
+          placeholder={props.data}
+          onChange={handleChange}
+        ></input>
+        <button onClick={updateField}>Update</button>
       </div>
     );
   }
