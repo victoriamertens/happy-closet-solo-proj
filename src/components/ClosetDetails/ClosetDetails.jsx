f914e43dbd29f90045d79ab524ee7e0d074b7c46
Victoria Mertens <victoria.mertens44@gmail.com>
2023-01-02 11:05:59 -0600

    Rednering detils page properly

--- src/components/ClosetDetails/ClosetDetails.jsx
+++ src/components/ClosetDetails/ClosetDetails.jsx
@@ -1,16 +1,28 @@
 import { useParams } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 
 function ClosetDetails() {
   const dispatch = useDispatch();
   const { id } = useParams();
+  const store = useSelector((store) => store.itemDetails);
+  console.log(store);
 
   useEffect(() => {
     dispatch({ type: 'GET_DETAILS', payload: id });
     console.log('in UseEffect');
   }, []);
-
-  return <p>Testing Closet Details: {id}</p>;
+  if (!store) {
+    return <p>Collecting your item's details</p>;
+  }
+  return (
+    <div class="item-details">
+      <p class="name">{store.name}</p>
+      <img src={store.image_url}></img>
+      <p>Cost: {store.cost}</p>
+      <p>Color: {store.color}</p>
+      <p>Category: {store.category}</p>
+    </div>
+  );
 }
 export default ClosetDetails;
