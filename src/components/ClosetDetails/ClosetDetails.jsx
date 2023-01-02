6509d6f69837532ffe77c5dfa82a9d172981d4ce
Victoria Mertens <victoria.mertens44@gmail.com>
2023-01-02 10:28:41 -0600

    UseEffect and dispatch hooked up. UseEffect functional. SAGAs needs to be written.

--- src/components/ClosetDetails/ClosetDetails.jsx
+++ src/components/ClosetDetails/ClosetDetails.jsx
@@ -1,7 +1,15 @@
 import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
 
 function ClosetDetails() {
+  const dispatch = useDispatch();
   const { id } = useParams();
+
+  useEffect(() => {
+    dispatch({ type: 'GET_DETAILS', payload: id });
+    console.log('in UseEffect');
+  }, []);
 
   return <p>Testing Closet Details: {id}</p>;
 }
