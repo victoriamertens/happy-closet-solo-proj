384cf6b990a274a68a974aa6607d05f78c0b1f19
Victoria Mertens <victoria.mertens44@gmail.com>
2023-01-04 09:42:35 -0600

    Posting outfit component is now editable, meaning you can accurately remove items from outfit builder.

--- src/components/MakeOutfitItem/MakeOutfitItem.jsx
+++ src/components/MakeOutfitItem/MakeOutfitItem.jsx
@@ -1,9 +1,25 @@
-import { useDispatch } from 'react-redux';
-import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useState, useEffect } from 'react';
 
 function MakeOutfitItem({ item }) {
   const dispatch = useDispatch();
-  const [clicked, setClicked] = useState(false);
+  const [inOutfit, setInOutfit] = useState(false);
+  const outfits = useSelector((store) => store.newOutfit);
+
+  useEffect(() => {
+    console.log('In useEffect');
+    setInOutfit(false);
+    checkInOutfit();
+  }, [outfits]);
+
+  function checkInOutfit() {
+    for (let outfitItem of outfits) {
+      console.log('outfititem:', outfitItem.id, item.id);
+      if (outfitItem.id === item.id) {
+        setInOutfit(true);
+      }
+    }
+  }
 
   const addOutfitToStore = () => {
     console.log('Adding to store:', item.id);
@@ -13,13 +29,17 @@ function MakeOutfitItem({ item }) {
     });
     setClicked(!clicked);
   };
-  if (clicked) {
+  if (!outfits) {
+    return <p>Loading...</p>;
+  }
+  if (inOutfit) {
     return <></>;
   }
   return (
     <div onClick={addOutfitToStore} key={item.id}>
       <p>Name: {item.name}</p>
       <img src={item.image_url}></img>
+      {console.log('outfit:', inOutfit)}
     </div>
   );
 }
