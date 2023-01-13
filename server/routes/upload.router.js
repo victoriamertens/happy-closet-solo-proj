a021ecbfa8fefa5f304bba2d11a13c59e580de98
Victoria Mertens <victoria.mertens44@gmail.com>
2023-01-13 14:50:19 -0600

    Refactored upload router

--- server/routes/upload.router.js
+++ server/routes/upload.router.js
@@ -1,32 +1,20 @@
+//Express and router imports
 const express = require('express');
-//const pool = require('../modules/pool');
 const router = express.Router();
-
+//Importing multer for file middleware
 const multer = require('multer');
+//Importing fs to open stream to file system of computer
 const fs = require('fs');
-//const cors = require('cors');
-//const app = express();
-//app.use(cors());
-
-// Import required AWS SDK clients and commands for Node.js.
+//using aws dev kit to handle the upload process
 const AWS = require('aws-sdk');
 const S3 = new AWS.S3();
-//const clientS3 = require('@aws-sdk/client-s3');
-//const { s3Client } = require('./libs/s3Client.js'); // Helper function that creates an Amazon S3 service client module.
-//const { path } = require('path');
-//const { fs } = require('fs');
+
+//importing key and secret key
 require('dotenv').config();
 
-let fileNameTest = '';
 //MULTER BELOW
-let storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './server/photos');
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname);
-  },
-});
+let storage = multer.diskStorage({});
+//using disk storage's default setting to store file in memory
 let upload = multer({ storage: storage });
 
 //AWS Configure for access keys
@@ -36,6 +24,7 @@ AWS.config.update({
   region: 'us-east-2',
 });
 
+//Below is testing proper credentials
 AWS.config.getCredentials(function (err) {
   if (err) console.log(err.stack);
   // credentials not loaded
@@ -48,19 +37,13 @@ router.post('/', upload.single('file'), async (req, res) => {
   // POST route code here
   try {
     // Initialize a "Read stream" to start
-    // reading data from this file
     let file = req.file;
-    console.log(`uploading ${file.filename} from ${file.path}...`);
     let fileStream = fs.createReadStream(file.path);
 
     fileStream.on('open', () => {
-      console.log(`stream is ${JSON.stringify(fileStream)}...`);
-
       uploadParams = {
         Bucket: 'myclosetbucket234q4623434532',
-        // Add the required 'Key' parameter using the 'path' module.
         Key: 'closetimages/' + file.filename,
-        // Add the required 'Body' parameter
         Body: fileStream,
         ACL: 'public-read',
         ContentType: 'image/jpeg',
@@ -68,12 +51,9 @@ router.post('/', upload.single('file'), async (req, res) => {
 
       S3.putObject(uploadParams, function (err, data) {
         if (err) console.log(err, err.stack);
-        // an error occurred, add fs.unlink for file?
         else {
-          console.log('Success, Here is the data', data);
           let imageUrl = `https://myclosetbucket234q4623434532.s3.us-east-2.amazonaws.com/closetimages/${file.filename}`;
           console.log('Image URL:', imageUrl);
-          //here is where you can put fs.unlink
           res.send(imageUrl);
         }
       });
